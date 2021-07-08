d61788b8c204f5a5440ce8dfbe8f462edc691d26
Nico Carlson <nicocarlson285@gmail.com>
2021-07-08 15:58:17 -0500

    Router and React Bootstrap Libraries Added

---
+++ src/component/MainComponent.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Main extends Component {
+  render() {
+    return (
+      <div>
+        <ul>
+            <li>Nav</li>
+            <li>Header</li>
+            <li>Redirect to Donut Menu</li>
+            <li>Redirect to Drink Menu</li>
+            <li>Redirect to Cart/Checkout</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Main;
