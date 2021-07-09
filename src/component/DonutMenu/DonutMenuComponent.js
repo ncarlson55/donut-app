8f41e20f4e62688fb6824a1482b1e7ecb2f72022
Nico Carlson <nicocarlson285@gmail.com>
2021-07-09 06:39:56 -0500

    Reacstrap Cards to DOnutMenu, working on grid issue

--- src/component/DonutMenu/DonutMenuComponent.js
+++ src/component/DonutMenu/DonutMenuComponent.js
@@ -1,14 +1,32 @@
 import React from 'react';
+import {
+  Row,
+  Col,
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  Button,
+} from 'reactstrap';
 
 function DonutMenu(props) {
-  console.log(props.donuts);
   return (
     <div>
-      {props.donuts.map((donut) => (
-        <li key={donut.id}>
-          {donut.name}, {donut.price}
-        </li>
-      ))}
+      <Row>
+        <Col xs="12" sm="4" lg="3">
+          {props.donuts.map((donut) => (
+            <Card className="text-center" body outline color="secondary">
+              <CardImg top width="100%" src={donut.image} alt="Donuts" />
+              <CardBody>
+                <CardTitle tag="h6">{donut.name}</CardTitle>
+                <CardText>${donut.price}</CardText>
+                <Button>Add To Cart</Button>
+              </CardBody>
+            </Card>
+          ))}
+        </Col>
+      </Row>
     </div>
   );
 }
