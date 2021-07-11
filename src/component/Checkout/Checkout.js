bb949a8222dc6fa088174b61a6feb97f1e99afed
Nico Carlson <nicocarlson285@gmail.com>
2021-07-11 00:02:27 -0500

    Checkout component pricing div complete

--- src/component/Checkout/Checkout.js
+++ src/component/Checkout/Checkout.js
@@ -1,8 +1,17 @@
 import React from 'react';
-import { Button } from 'reactstrap';
+import { Container, Row, Col, Button } from 'reactstrap';
 
 function Checkout(props) {
   const { cartItems, onAdd, onRemove } = props;
+  //Variables for setting up Checkout 
+  const itemsPrice = cartItems.reduce(
+    (acc, curr) => acc + curr.price * curr.qty,
+    0
+  );
+  const tax = itemsPrice * 0.08;
+  const deliveryPrice = itemsPrice > 50 ? 0 : 5.99;
+  const totalPrice = itemsPrice + tax + deliveryPrice;
+
   return (
     <div>
       <h2>Cart Items</h2>
@@ -15,10 +24,30 @@ function Checkout(props) {
             <Button onClick={() => onRemove(item)}>-</Button>
           </div>
           <div>
-              {item.qty} x ${item.price.toFixed(2)}
+            {item.qty} x ${item.price.toFixed(2)}
           </div>
         </div>
       ))}
+      {cartItems.length !== 0 && (
+        <Container>
+          <Row>
+            <Col xs="2">Item Price</Col>
+            <Col xs="10">${itemsPrice.toFixed(2)}</Col>
+          </Row>
+          <Row>
+            <Col xs="2">Tax Price</Col>
+            <Col xs="10">${tax.toFixed(2)}</Col>
+          </Row>
+          <Row>
+            <Col xs="2">Delivery Price</Col>
+            <Col xs="10">${deliveryPrice.toFixed(2)}</Col>
+          </Row>
+          <Row>
+            <Col xs="2">Total Price</Col>
+            <Col xs="10">${totalPrice.toFixed(2)}</Col>
+          </Row>
+        </Container>
+      )}
     </div>
   );
 }
